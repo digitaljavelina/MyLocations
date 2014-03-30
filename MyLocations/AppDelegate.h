b031ec77655fabd927a677d589122145e0ca9d41
Michael Henry <mididoc@cox.net>
2014-03-29 22:47:54 -0700

    Initial Commit

---
+++ MyLocations/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyLocations
+//
+//  Created by Michael Henry on 3/29/14.
+//  Copyright (c) 2014 Digital Javelina, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyLocations/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyLocations
+//
+//  Created by Michael Henry on 3/29/14.
+//  Copyright (c) 2014 Digital Javelina, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
