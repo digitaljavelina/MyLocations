6bf585b3213bec94f6c4a880951d2ab7332d1b0a
Michael Henry <mididoc@cox.net>
2014-03-30 21:43:45 -0700

    Initial commit - section 1

---
+++ MyLocations/CurrentLocationViewController.h
@@ -0,0 +1,23 @@
+//
+//  CurrentLocationViewController.h
+//  MyLocations
+//
+//  Created by Michael Henry on 3/29/14.
+//  Copyright (c) 2014 Digital Javelina, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface CurrentLocationViewController : UIViewController <CLLocationManagerDelegate>
+
+@property (nonatomic, weak) IBOutlet UILabel *messageLabel;
+@property (nonatomic, weak) IBOutlet UILabel *latitudeLabel;
+@property (nonatomic, weak) IBOutlet UILabel *longitudeLabel;
+@property (nonatomic, weak) IBOutlet UILabel *addressLabel;
+@property (nonatomic, weak) IBOutlet UIButton *tagButton;
+@property (nonatomic, weak) IBOutlet UIButton *getButton;
+
+- (IBAction)getLocation:(id)sender;
+
+@end
