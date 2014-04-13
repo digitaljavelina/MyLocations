4235a1a00cbbcf44fa42638b6e3e9ed97592211f
Michael Henry <mididoc@cox.net>
2014-04-13 10:39:40 -0700

    Finished implementation of category picker and unwind segue

--- MyLocations/CategoryPickerViewController.m
+++ MyLocations/CategoryPickerViewController.m
@@ -75,4 +75,14 @@
     }
 }
 
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    if ([segue.identifier isEqualToString:@"PickedCategory"]) {
+        UITableViewCell *cell = sender;
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
+        self.selectedCategoryName = _categories[indexPath.row];
+    }
+}
+
+
 @end
--- MyLocations/LocationDetailsViewController.m
+++ MyLocations/LocationDetailsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LocationDetailsViewController.h"
+#import "CategoryPickerViewController.h"
 
 @interface LocationDetailsViewController () <UITextViewDelegate>
 
@@ -22,12 +23,14 @@
 @implementation LocationDetailsViewController {
     
     NSString *_descriptionText;
+    NSString *_categoryName;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
     
     if ((self = [super initWithCoder:aDecoder])) {
         _descriptionText = @"";
+        _categoryName = @"No Category";
     }
     
     return self;
@@ -38,7 +41,7 @@
     [super viewDidLoad];
     
     self.descriptionTextView.text = _descriptionText;
-    self.categoryLabel.text = @"";
+    self.categoryLabel.text = _categoryName;
     self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", self.coordinate.latitude];
     self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", self.coordinate.longitude];
     
@@ -82,9 +85,24 @@
     
 }
 
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    if ([segue.identifier isEqualToString:@"PickCategory"]) {
+        CategoryPickerViewController *controller = segue.destinationViewController;
+        controller.selectedCategoryName = _categoryName;
+    }
+}
+
 - (void)closeScreen {
     
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)categoryPickerDidPickCategory:(UIStoryboardSegue *)segue {
+    
+    CategoryPickerViewController *viewController = segue.sourceViewController;
+    _categoryName = viewController.selectedCategoryName;
+    self.categoryLabel.text = _categoryName;
 }
 
 #pragma mark - UITableViewDelegate
