441547a0b1a227110fff198a866ee95b51d20338
Michael Henry <mididoc@cox.net>
2014-04-13 17:11:24 -0700

    Added HUD when location is tagged

--- MyLocations/LocationDetailsViewController.m
+++ MyLocations/LocationDetailsViewController.m
@@ -8,6 +8,7 @@
 
 #import "LocationDetailsViewController.h"
 #import "CategoryPickerViewController.h"
+#import "HudView.h"
 
 @interface LocationDetailsViewController () <UITextViewDelegate>
 
@@ -52,6 +53,23 @@
     }
     
     self.dateLabel.text = [self formatDate:[NSDate date]];
+    
+    UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard:)];
+    
+    gestureRecognizer.cancelsTouchesInView = NO;
+    [self.tableView addGestureRecognizer:gestureRecognizer];
+}
+
+- (void)hideKeyboard:(UIGestureRecognizer *)gestureRecognizer {
+    
+    CGPoint point = [gestureRecognizer locationInView:self.tableView];
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:point];
+    
+    if (indexPath != nil && indexPath.section == 0 && indexPath.row == 0) {
+        return;
+    }
+    
+    [self.descriptionTextView resignFirstResponder];
 }
 
 - (NSString *)stringFromPlacemark:(CLPlacemark *)placemark {
@@ -73,9 +91,10 @@
 
 - (IBAction)done:(id)sender {
     
-    NSLog(@"Description '%@'", _descriptionText);
+    HudView *hudView = [HudView hudInView:self.navigationController.view animated:YES];
+    hudView.text = @"Tagged";
     
-    [self closeScreen];
+    [self performSelector:@selector(closeScreen) withObject:nil afterDelay:0.6];
     
 }
 
@@ -123,6 +142,22 @@
         return self.addressLabel.frame.size.height + 20;
     } else {
         return 44;
+    }
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (indexPath.section == 0 || indexPath.section == 1) {
+        return indexPath;
+    } else {
+        return nil;
+    }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (indexPath.section == 0 && indexPath.row == 0) {
+        [self.descriptionTextView becomeFirstResponder];
     }
 }
 
